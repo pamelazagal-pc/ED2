908703b59c11420547da1b3f92f0929d8ed7a578
UriJahen <mjuo230383@upemor.edu.mx>
2026-03-19 14:50:30 -0600

    uri

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,47 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\PedidoController;
+
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('pedido', comidaController::class);
 });
 
 Rout::get('registro', [
     AuthController::class, 'registerForm'
 ])->name('registro');
+
+//ruta para registrar usuarios
+Route::post('/registro', [ //el post es para mandar informacion, el get solo para solicitar la vista
+    AuthController::class, 'register'
+])->name('registro.store'); //el .store ahorita no es mas que una referencia para saber que significa guardar
+
+//ruta para regresar vista de inicio de sesion
+Route::get('/acceso', [
+    AuthController::class, 'loginForm'
+])->name('acceso');
+
+//ruta para iniciar sesion
+Route::post('/acceso', [
+    AuthController::class, 'login'
+])->name('acceso.store');
+
+//ruta para cerrar sesion
+Route::post('/cerrar', [
+    AuthController::class, 'logout'
+])->name('cerrar');
+
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/admin.dashboard', [
+        AuthController::class, 'adminDashboard'
+    ])->name('admin.dashboard');
+
+});
