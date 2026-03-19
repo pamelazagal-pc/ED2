2e2a3aac7e7fc83344f13f3dff6c892cde6bf864
davjax11 <carnalpasalascervezas@gmail.com>
2026-03-19 14:35:33 -0600

    Model user

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,10 +20,18 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'age',
+        'phone',
+        'direction',
+        'is_admin',
         'email',
         'password',
     ];
 
+    protected $casts = [
+        'is_admin' => 'boolean',
+    ];
+    
     /**
      * The attributes that should be hidden for serialization.
      *
